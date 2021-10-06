52e33ad49f79ccad929248e23b89636ed52eaf99
berkaldanmaz <aldanmazberk@gmail.com>
2021-10-06 20:55:59 +0100

    Basic Calculator

---
+++ app/src/main/java/com/aldanmaz/berkcalculator/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.aldanmaz.berkcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var numberOne: Int? = null
+    var numberTwo: Int? = null
+    var result: Int? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun mySum(view: View) {
+        numberOne = numberOneText.text.toString().toIntOrNull()
+        numberTwo = numberTwoText.text.toString().toIntOrNull()
+        if (numberOne == null || numberTwo == null) {
+            Toast.makeText(applicationContext, "Give me Number!", Toast.LENGTH_LONG).show()
+        } else {
+
+            result = numberOne!! + numberTwo!!
+            resultText.text = "Result: $result"
+        }
+    }
+
+
+
+
+
+        fun mySub(view: View) {
+
+         numberOne = numberOneText.text.toString().toIntOrNull()
+         numberTwo = numberTwoText.text.toString().toIntOrNull()
+        if (numberOne == null || numberTwo == null) {
+            resultText.text = "Error!"
+        } else {
+            result = numberOne!! - numberTwo!!
+            resultText.text = "Result: $result"
+        }
+        }
+
+        fun myMultiply(view: View) {
+            numberOne = numberOneText.text.toString().toIntOrNull()
+            numberTwo = numberTwoText.text.toString().toIntOrNull()
+            if(numberOne == null || numberTwo == null ) {
+                Toast.makeText(applicationContext,"Give me my Number!",Toast.LENGTH_LONG).show()
+            } else {
+                result = numberOne!! * numberTwo!!
+
+                resultText.text = "Result: $result"
+            }
+
+
+        }
+
+        fun myDiv(view: View) {
+            numberOne = numberOneText.text.toString().toIntOrNull()
+            numberTwo = numberTwoText.text.toString().toIntOrNull()
+            if(numberOne == null || numberTwo == null) {
+                resultText.text = "Error!"
+            } else {
+                result = numberOne!! / numberTwo!!
+
+                resultText.text = "Result: $result"
+            }
+
+        }
+
+    }
+
